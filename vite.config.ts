97948c093d9a58b9d85a8db6aa0abe3c7d543771
FiNeX96 <rodrigoaguiar96@ua.pt>
2024-05-01 09:37:40 +0100

    PHIH-14 dockerize patient frontend

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 export default defineConfig({
   plugins: [react(), TanStackRouterVite(),],
+  server: {
+    host:'0.0.0.0',
+    watch: {
+      usePolling: true
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
